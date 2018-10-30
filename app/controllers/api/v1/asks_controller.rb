241aa87e69ebd572702d1e686078f7cfb18f58a3
Jay Pinho <jaypinho@gmail.com>
2018-10-30 16:58:25 -0400

    Start responding to incoming requests

--- app/controllers/api/v1/asks_controller.rb
+++ app/controllers/api/v1/asks_controller.rb
@@ -3,9 +3,28 @@ class Api::V1::AsksController < ApplicationController
   def challenge
     if params.has_key?(:challenge)
       render json: {"challenge": params[:challenge]}
-    else
-      render json: {"yo": "hey"}
+      return
     end
+
+    if params['event']['type'] == 'app_mention'
+
+      # Book a meeting
+      if params['event']['text'].strip.downcase.match(/book (?:me\s)a meeting (for\s)?(today|this morning|this afternoon|tomorrow|tomorrow morning|tomorrow afternoon)/)
+        render json: {"answer": "ok, booked!"}
+
+      # Is there a room free right now?
+      elsif params['event']['text'].strip.downcase.match(/(?:is there a|what) (?:conference\s|meeting\s)?rooms? (?:are\s)*(?:free|available|open) (?:now|right now|at the moment)/)
+        render json: {"answer": "nah, everything's full"}
+
+      # When is Person X's next free slot?
+    elsif params['event']['text'].strip.downcase.match(/when is @(\w+) (?:free|available|open) next/)
+        given_user = params['event']['text'].strip.downcase.match(/when is @(\w+) (?:free|available|open) next/)[1]
+        render json: {"answer": "checking @#{given_user}'s availability"}
+
+      end
+
+    end
+
   end
 
 end
